5503dd6257e09f54604befa6909b17810273b6ec
zaidbhimani <zaidbhimani786@gmail.com>
2017-09-10 12:53:46 +0530

    prctise section

--- todolist/custom.js
+++ todolist/custom.js
@@ -1,18 +1,37 @@
 $(document).ready(function() {
+  var statement = "plz mention your task";
+  
   $(".ad").on("click", function() {
-    var text = $("#int").val();
-
+  
+  var text = $("#int").val();
+  
+  if($("#int").val().length > 0){
     $(".new-text").append(
-      "<li>" + text + ' <button class="del">delete</button></li>'
+      '<li> <span> <span class="text">' + text + ' </span><button class="edit">Edit</button> <button class="del">delete</button></span></li>'
     );
-    $("#int").val("");
+    $(".stat").text("");
+  }
+  else{
+    $(".stat").text(statement)
+  }
+   
+   $("#int").val("");
   });
+
+ $("#int").on("click", function() {
+$(".stat").html("");
+
+
+ }); 
 
   $(".new-text").on("click", ".del", function(evt) {
     $(evt.target)
-      .parent()
+      .closest("li")
       .remove();
   });
+
+
+
 
   $(".done").on("click", function(evt) {
     // $(evt.target).toggleClass("active");
@@ -23,11 +42,7 @@ $(document).ready(function() {
      $(evt.target).removeClass("active")
     } else {
      $(evt.target).addClass("active")
-
     }
-
-    
-
     //           wrong code
     //   if ($(evt.target).css("text-decoration") == "line-through") {
     //     $(evt.target).css({ "text-decoration": "none" });
@@ -36,4 +51,82 @@ $(document).ready(function() {
     //     $(evt.target).css({ "text-decoration": "line-through" });
     //   }
   });
+
+$(".new-text").on("click", '.edit', function(evt) {
+  
+  //Get parent element
+  var $li = $(evt.target).closest("li");
+
+  //Old Text
+  var $litext = $li.find(".text").html();
+  
+  //hide all content in li
+  $li.find("> span").hide();
+
+
+  //new Input Tag
+  $li.append('<input class="editform" type="text" value="' + $litext +'" /><button class="editBtn">Save</button')
+
+
+  $(".editBtn").on("click", function(){
+    // Previous Content SHow
+    $li.find("> span").show();
+
+    // edit for hide
+    $(".editform").hide();
+    $(".editBtn").hide();
+     
+    // value to set
+    var zaid = $(".editform").val(); 
+    $li.find(".text").html(zaid);
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //new Button Tag and Append
+  /*
+  var $savebtn = $("<button />", {
+    html:"save",
+    class:"saveBtn",
+    on:{
+      click:function(){
+        //get New text
+        var newtext =  $li.find("input").val();;
+        
+        //Remove Edit Form we dont need it  
+        $li.find("input.editform").remove();
+        $li.find("button.saveBtn").remove();
+        
+        //unhide all content and set new text to li
+        $li.find("> span").show();
+        
+        //set text to input
+        $li.find(".text").html(newtext)
+      }
+    }
+  })
+
+
+   $li.append($savebtn)
+  */
+  
+
+
+});
+
+
 });
---
+++ todolist/practise/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+$(".ad").on("click" , function(){
+
+var text=$("#inp").val();
+
+if($("#inp").val().length > 0){
+	
+	$(".content-list").append('<li>'+text+'<button class="edt">edit</button><button class="del">delete</button></li>');
+}
+
+else{
+	var stat="plz enter your task";
+    $(".caution").text(stat);
+}
+$("#inp").val("");
+});
+
+$("#inp").on("click" , function(){
+$(".caution").text("");
+});
+
+$(".content-list").on("click", ".del" ,function(evt){
+(evt.target).closest("li").remove()
+});
+
+$(".content-list").on("click", ".edt" ,function(evt){
+})
+
+});//main ending

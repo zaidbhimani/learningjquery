404468d411ef320c0b13e32f69d34e18eca920fc
zaidbhimani <zaidbhimani786@gmail.com>
2017-09-18 15:45:06 +0530

    add

--- image slider using jquery/custom.js
+++ image slider using jquery/custom.js
@@ -1,10 +1,7 @@
-$(document).ready(function(){
-$(".img1").fadeIn(500)
-$(".img1").delay(10000)
-$(".img1").fadeOut()
+$(document).ready(function() {
+	$(".img1").fadeIn(500);
+	$(".img1").delay(10000);
+	$(".img1").fadeOut();
 
-// $(".img1").delay(500).hide("slide",{direction:'left'},500);
-
-
-
+	// $(".img1").delay(500).hide("slide",{direction:'left'},500);
 });

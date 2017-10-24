47d11daf5620f4632cc23d6b6de65d54a2aa4695
zaidbhimani <zaidbhimani786@gmail.com>
2017-10-24 15:58:56 +0530

    completed-binding

--- binding multiple event/custom.js
+++ binding multiple event/custom.js
@@ -1,32 +1,35 @@
 $(document).ready(function(){
 
-$('#btnClickMe').bind('click mouseOver',function(evt){
+$('#btnClickMe').bind('click mouseover mouseout',function(evt){
 
-// if(evt.type=='click'){
-// 	$('#divResult').html('X='+evt.pageX+ ', Y='+evt.pageY);
-// }
+if(evt.type=='click'){
+	$('#divResult').html('X='+evt.pageX+ ', Y='+evt.pageY);
+}
 
-// else if (evt.type=='mouseOver'){
-// 	$(this).addClass('ButtonStyle');
-// }
+else if (evt.type=='mouseover'){
+	$(this).addClass('ButtonStyle');
+}
 
-// if{
+else{
 
-// 	$(this).removeClass('ButtonStyle');
-// }
-
-if (event.type == 'click') {
-                    $('#divResult').html('Button Clicked at '
-                        + 'X = ' + event.pageX + ' Y = ' + event.pageY);
-                }
-                else if (event.type == 'mouseover') {
-                    $(this).addClass('ButtonStyle');
-                }
-                else {
-                    $(this).removeClass('ButtonStyle');
-                }
-
+	$(this).removeClass('ButtonStyle');
+}
 
 });
 
+$('#btnEnableMouseOverEffect').on('click', function(){
+	$('#btnClickMe').bind('mouseover' , function(){
+     $(this).addClass('ButtonStyle');
+	});
 });
+
+$('#btnDisableMouseOverEffect').on('click', function(){
+	$('#btnClickMe').unbind('mouseover')
+});
+
+
+
+
+
+
+});//main ending
